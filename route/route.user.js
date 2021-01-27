89df78b3561f6a548bf5852fb2c9cb4b1684e00d
zeroo <anhtuan090795@gmail.com>
2021-01-27 11:03:08 +0700

    middleware function

--- route/route.user.js
+++ route/route.user.js
@@ -4,10 +4,11 @@ var router = express.Router();
 var controller = require('../controller/controll.user');
 var db = require('../db');
 var shortid = require('shortid');
+var validate = require('../validate/user.validate.js');
 
 router.get('/', controller.index);
 router.get('/create', controller.create);
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 router.get('/:id', controller.id);
 router.get('/search', controller.search);
 
