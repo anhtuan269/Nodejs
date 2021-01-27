251c6eb56db44113f1ea857c39d7adab417170b5
zeroo <anhtuan090795@gmail.com>
2021-01-27 10:34:21 +0700

    login

--- controller/controll.user.js
+++ controller/controll.user.js
@@ -11,6 +11,21 @@ module.exports.create =  function(req, res ){
 };
 module.exports.postCreate = function( req, res){
     req.body.id = shortid.generate();
+    var errors = [];
+    if (!req.body.name) {
+        errors.push('Name is required');
+    }
+
+    if (!req.body.phone) {
+        errors.push('phone is required');
+    }
+    
+    if (errors.lenght) {
+       res.render('users/create', {
+           errors: errors
+       });
+       return;
+    }
     db.get('users').push(req.body).write();
     res.redirect('/users');
     
