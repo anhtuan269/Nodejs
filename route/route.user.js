4d42facef472ba0dfcb61b3c1b7f698c0c94b4f0
zeroo <anhtuan090795@gmail.com>
2021-01-29 12:13:44 +0700

    uploadfile

--- route/route.user.js
+++ route/route.user.js
@@ -8,13 +8,19 @@ var shortid = require('shortid');
 var validate = require('../validate/user.validate.js');
 var authMiddleware = require('../middlewares/auth.middleware');
 
+var upload = multer ({dest: './public/uploads '});
+
 router.get('/', controller.index);
 router.get('/create', controller.create);
 router.get('/cookie', function(req, res, next ){
     res.cookie('user-id', 123435678910);
     res.send('hello ');
 });
-router.post('/create', validate.postCreate, controller.postCreate);
+router.post('/create',
+    upload.single('avatar'),
+    validate.postCreate, 
+    controller.postCreate
+);
 router.get('/:id', controller.id);
 router.get('/search', controller.search);
 
